a587de1c2fdbfa1db6027df62837d1417160d851
System Administrator <root@Zhipeng.home>
2021-10-13 03:01:56 -0400

    Lab 6

--- caesar.cpp
+++ caesar.cpp
@@ -6,7 +6,7 @@ char shiftChar(char c, int rShift){
 	
 	char resultingChar = chShifted;
 	if (c >= 65 && c <= 90){
-		if (chShifted > 90) resultingChar = 65 + ((chShifted%91)%26);	//%90 represents "remainder" from max code point and %26 represents remainder from alphabetical cycle
+		if (chShifted > 90) resultingChar = 65 + ((chShifted%91)%26);	//%91 represents "remainder" from max code point and %26 represents remainder from alphabetical cycle
 		
 	}
 	else if (c >= 97 && c <= 122){
